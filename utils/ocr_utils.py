0513f2fa7fe41ad8fe52c31ac01faebdc6c0eb0b
Zouhairihouda1 <houdazouhairi24@gmail.com>
2025-11-25 23:30:16 +0000

    feat:ocr(outils)

---
+++ utils/ocr_utils.py
@@ -0,0 +1,13 @@
+print("ici je metter le code OCR utils")
+def preprocess_image(image):
+    # Code to preprocess the image for OCR
+    pass
+def extract_text(image):
+    # Code to extract text from the image using OCR
+    pass
+def postprocess_text(text):
+    # Code to clean and format the extracted text
+    pass
+
+def ocr_pipeline(image):
+    
